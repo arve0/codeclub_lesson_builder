edaec9034691690cdbbf03df6b489c7135201fde
arve0 <arve.seljebu@gmail.com>
2015-02-20 19:41:30 +0100

    fix url logo

--- utils/insert_yaml.sh
+++ utils/insert_yaml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # example usage:
-# find nb-NO/python/codeclubUK -name [a-z]*md | xargs -L 1 insert.sh
+# find src/python/codeclubUK -name "*.md" -print0 | xargs -0 -L 1 insert_yaml.sh
 
 file=$1
 
@@ -15,7 +15,7 @@ sed -i '' '
   /\n$/ {
 # insert above
     i\
-logo: ../../../assets/img/ccuk_logo.png\
+logo: ../../assets/img/ccuk_logo.png\
 author: Oversatt fra [Code Club UK](//codeclub.org.uk)\
 license: "[Code Club World Limited Terms of Service](https://github.com/CodeClub/scratch-curriculum/blob/master/LICENSE.md)"
   }
