8dbd1e7de46d732427b0caaed56708071b0dcf57
Ole Kristian Pedersen <okpedersen@gmail.com>
2015-06-12 17:20:57 +0200

    Course name fix

--- scripts/index.js
+++ scripts/index.js
@@ -13,9 +13,9 @@ $('li.playlist').click(function(){
  * show/hide course info
  */
 $('h1.info').click(function(){
-  console.log('clicked me')
-  $('.info-' + this.id).slideToggle();
-})
+  console.log('clicked me');
+  $('.content').slideToggle();
+});
 
 /*
  * tooltips
