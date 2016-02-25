a78f25b0258bfcd9ee76671230431522dbfac66f
arve0 <arve.seljebu@gmail.com>
2016-02-25 18:37:34 +0100

    tools: use standard code style

--- tools.js
+++ tools.js
@@ -1,48 +1,46 @@
 // read front matter on demand
-var matter = require('gray-matter');
-var path = require('path');
-var fs = require('fs');
-var config = require('./config.js');
+var matter = require('gray-matter')
+var path = require('path')
+var fs = require('fs')
+var config = require('./config.js')
 
 /** Returns true if file exists */
-function isFile(dir, file){
-  var fullPath = path.join(config.sourceRoot, dir, file);
+function isFile (dir, file) {
+  var fullPath = path.join(config.sourceRoot, dir, file)
   try {
-    fs.statSync(fullPath);
+    fs.statSync(fullPath)
   } catch (e) {
-    return false;
+    return false
   }
-  return true;
+  return true
 }
 
 /** read front-matter from file, fail gracefully. */
-function frontmatter(filename) {
-  var filepath = path.join(config.lessonRoot, config.sourceFolder, filename);
-  var m;
+function frontmatter (filename) {
+  var filepath = path.join(config.lessonRoot, config.sourceFolder, filename)
+  var m
   try {
-    m = matter.read(filepath);
+    m = matter.read(filepath)
   } catch (e) {
-    return {};
+    return {}
   }
-  return m.data;
+  return m.data
 }
-
 
 /**
  * remove files from build which have external set
  */
-function removeExternal(files, _, done) {
-  Object.keys(files).forEach(function(key){
+function removeExternal (files, _, done) {
+  Object.keys(files).forEach(function (key) {
     if (files[key].external) {
-      delete files[key];
+      delete files[key]
     }
-  });
-  done();
+  })
+  done()
 }
-
 
 module.exports = {
   isFile: isFile,
   frontmatter: frontmatter,
   removeExternal: removeExternal
-};
+}
