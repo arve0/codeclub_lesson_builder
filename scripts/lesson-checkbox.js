8772ae6386aa7c0142d3d7ba91973df57e48e0da
arve0 <arve.seljebu@gmail.com>
2017-06-05 20:17:38 +0200

    fix click handler for checkbox

--- scripts/lesson-checkbox.js
+++ scripts/lesson-checkbox.js
@@ -1,4 +1,6 @@
-const SELECTOR = 'p > input'
+const SELECTOR = 'ul > li'
+// style apply to input, so safe to set class checked on all list items
+// nice when we have `- [ ]` inside `.flag` or similar
 
 $(SELECTOR).click(toggleChecked)
 
